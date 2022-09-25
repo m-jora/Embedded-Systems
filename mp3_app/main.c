0689d1719b23e0502ea22513cd08d06acb4c4267
Garrett Mason <gmmncf@mst.edu>
2022-09-25 18:33:41 -0500

    Project init

---
+++ mp3_app/main.c
@@ -0,0 +1,18 @@
+/*
+ * mp3_app.c
+ *
+ * Created: 9/25/2022 6:31:35 PM
+ * Author : garre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
